129ac58c7ff7fd90564d9c56967ca5133307a342
Keiichi Watanabe <udon.watanabe@gmail.com>
2020-05-29 19:42:55 +0900

    clippy: fix clippy::match_bool

--- src/device.rs
+++ src/device.rs
@@ -68,9 +68,10 @@ impl Device {
 
         let flags = OFlag::O_RDWR
             | OFlag::O_CLOEXEC
-            | match config.non_blocking_dqbuf {
-                true => OFlag::O_NONBLOCK,
-                false => OFlag::empty(),
+            | if config.non_blocking_dqbuf {
+                OFlag::O_NONBLOCK
+            } else {
+                OFlag::empty()
             };
 
         let fd = open(path, flags, Mode::empty())?;
