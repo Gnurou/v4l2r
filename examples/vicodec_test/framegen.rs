8c453015771cfc868670e81098131319067e1661
Alexandre Courbot <gnurou@gmail.com>
2020-08-30 21:55:27 +0900

    vicodec_test/framegen: use same code as vicodec_pipeline

--- examples/vicodec_test/framegen.rs
+++ examples/vicodec_test/framegen.rs
@@ -4,16 +4,15 @@
 /// `bytes_per_line. `seed` can be increased over consecutive calls to animate
 /// the pattern.
 pub fn gen_pattern(frame: &mut [u8], bytes_per_line: usize, seed: u32) {
-    let width = bytes_per_line / 3;
-    let height = frame.len() / bytes_per_line;
-
-    (0..height)
-        .flat_map(move |y| (0..width).map(move |x| (x, y)))
-        .zip(frame.chunks_mut(3))
-        .for_each(|((x, y), pixel)| {
-            let rgba = seed.wrapping_add((x ^ y) as u32).to_le_bytes();
-            pixel[0] = rgba[0];
-            pixel[1] = rgba[1];
-            pixel[2] = rgba[2];
+    frame
+        .chunks_exact_mut(bytes_per_line)
+        .enumerate()
+        .for_each(|(y, line)| {
+            line.chunks_exact_mut(3).enumerate().for_each(|(x, pixel)| {
+                let rgba = seed.wrapping_add((x ^ y) as u32).to_le_bytes();
+                pixel[0] = rgba[0];
+                pixel[1] = rgba[1];
+                pixel[2] = rgba[2];
+            });
         });
 }
