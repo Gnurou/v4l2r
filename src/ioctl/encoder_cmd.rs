51cc78991626567e5ca5f8097d323972614a01e7
Alexandre Courbot <gnurou@gmail.com>
2020-10-24 10:47:12 +0900

    ioctl/encoder_cmd: add support for VIDIOC_TRY_ENCODER_CMD

--- src/ioctl/encoder_cmd.rs
+++ src/ioctl/encoder_cmd.rs
@@ -8,9 +8,10 @@ use thiserror::Error;
 mod ioctl {
     use crate::bindings::v4l2_encoder_cmd;
     nix::ioctl_readwrite!(vidioc_encoder_cmd, b'V', 77, v4l2_encoder_cmd);
+    nix::ioctl_readwrite!(vidioc_try_encoder_cmd, b'V', 78, v4l2_encoder_cmd);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum EncoderCommand {
     Start,
     Stop(bool),
@@ -28,25 +29,46 @@ pub enum EncoderCmdError {
     IoctlError(Error),
 }
 
+fn map_nix_error(error: nix::Error, command: EncoderCommand) -> EncoderCmdError {
+    match error {
+        Error::Sys(Errno::EBUSY) => EncoderCmdError::DrainInProgress,
+        Error::Sys(Errno::EINVAL) => EncoderCmdError::UnsupportedCommand(command),
+        e => EncoderCmdError::IoctlError(e),
+    }
+}
+
+impl From<EncoderCommand> for bindings::v4l2_encoder_cmd {
+    fn from(command: EncoderCommand) -> Self {
+        bindings::v4l2_encoder_cmd {
+            cmd: match &command {
+                EncoderCommand::Start => bindings::V4L2_ENC_CMD_START,
+                EncoderCommand::Stop(_) => bindings::V4L2_ENC_CMD_STOP,
+                EncoderCommand::Pause => bindings::V4L2_ENC_CMD_PAUSE,
+                EncoderCommand::Resume => bindings::V4L2_ENC_CMD_RESUME,
+            },
+            flags: match &command {
+                EncoderCommand::Stop(at_gop) if *at_gop => bindings::V4L2_ENC_CMD_STOP_AT_GOP_END,
+                _ => 0,
+            },
+            ..unsafe { mem::zeroed() }
+        }
+    }
+}
+
 pub fn encoder_cmd(fd: &impl AsRawFd, command: EncoderCommand) -> Result<(), EncoderCmdError> {
-    let mut enc_cmd = bindings::v4l2_encoder_cmd {
-        cmd: match &command {
-            EncoderCommand::Start => bindings::V4L2_ENC_CMD_START,
-            EncoderCommand::Stop(_) => bindings::V4L2_ENC_CMD_STOP,
-            EncoderCommand::Pause => bindings::V4L2_ENC_CMD_PAUSE,
-            EncoderCommand::Resume => bindings::V4L2_ENC_CMD_RESUME,
-        },
-        flags: match &command {
-            EncoderCommand::Stop(at_gop) if *at_gop => bindings::V4L2_ENC_CMD_STOP_AT_GOP_END,
-            _ => 0,
-        },
-        ..unsafe { mem::zeroed() }
-    };
+    let mut enc_cmd = bindings::v4l2_encoder_cmd::from(command);
 
     match unsafe { ioctl::vidioc_encoder_cmd(fd.as_raw_fd(), &mut enc_cmd) } {
         Ok(_) => Ok(()),
-        Err(Error::Sys(Errno::EBUSY)) => Err(EncoderCmdError::DrainInProgress),
-        Err(Error::Sys(Errno::EINVAL)) => Err(EncoderCmdError::UnsupportedCommand(command)),
-        Err(e) => Err(EncoderCmdError::IoctlError(e)),
+        Err(e) => Err(map_nix_error(e, command)),
+    }
+}
+
+pub fn try_encoder_cmd(fd: &impl AsRawFd, command: EncoderCommand) -> Result<(), EncoderCmdError> {
+    let mut enc_cmd = bindings::v4l2_encoder_cmd::from(command);
+
+    match unsafe { ioctl::vidioc_try_encoder_cmd(fd.as_raw_fd(), &mut enc_cmd) } {
+        Ok(_) => Ok(()),
+        Err(e) => Err(map_nix_error(e, command)),
     }
 }
