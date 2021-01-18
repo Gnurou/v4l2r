8867b19f4a62e38b017b9bfe4589b090ccc6f527
Alexandre Courbot <gnurou@gmail.com>
2021-01-18 23:06:04 +0900

    memory/dmabuf: add DMABufSource trait
    DMABuf handles can be created from various sources, not only files.
    Moreover we need to specify the length field of a V4L2 plane, and this
    trait allows us to request a method that provides this information.

--- src/memory/dmabuf.rs
+++ src/memory/dmabuf.rs
@@ -11,25 +11,48 @@ impl Memory for DMABuf {
 
 impl Imported for DMABuf {}
 
+pub trait DMABufSource: AsRawFd + Debug + Send {
+    fn len(&self) -> u64;
+
+    /// Make Clippy happy.
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+impl DMABufSource for std::fs::File {
+    fn len(&self) -> u64 {
+        match self.metadata() {
+            Err(_) => {
+                eprintln!("Failed to compute File size for use as DMABuf, using 0...");
+                0
+            }
+            Ok(m) => m.len(),
+        }
+    }
+}
+
 /// Handle for a DMABUF plane. Any type that can provide a file descriptor is
 /// valid.
 #[derive(Debug)]
-pub struct DMABufHandle<T: AsRawFd + Debug + Send>(pub T);
+pub struct DMABufHandle<T: DMABufSource>(pub T);
 
-impl<T: AsRawFd + Debug + Send> From<T> for DMABufHandle<T> {
+impl<T: DMABufSource> From<T> for DMABufHandle<T> {
     fn from(dmabuf: T) -> Self {
         DMABufHandle(dmabuf)
     }
 }
 
-impl<T: AsRawFd + Debug + Send + 'static> PlaneHandle for DMABufHandle<T> {
+impl<T: DMABufSource + 'static> PlaneHandle for DMABufHandle<T> {
     type Memory = DMABuf;
 
     fn fill_v4l2_plane(&self, plane: &mut bindings::v4l2_plane) {
         plane.m.fd = self.0.as_raw_fd();
+        plane.length = self.0.len() as u32;
     }
 
     fn fill_v4l2_splane_buffer(plane: &bindings::v4l2_plane, buffer: &mut bindings::v4l2_buffer) {
         buffer.m.fd = unsafe { plane.m.fd };
+        buffer.length = plane.length;
     }
 }
