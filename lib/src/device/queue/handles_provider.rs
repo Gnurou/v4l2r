e5e9e7bb554dc83eda4bde1cd1827edca21440fd
Alexandre Courbot <gnurou@gmail.com>
2021-02-14 16:35:19 +0900

    device: implement HandlesProvider on Box<HandlesProvider>
    Make it easy to use providers on the heap.

--- lib/src/device/queue/handles_provider.rs
+++ lib/src/device/queue/handles_provider.rs
@@ -22,6 +22,30 @@ pub trait HandlesProvider: Send + 'static {
     fn get_handles(&mut self, waker: &Arc<Waker>) -> Option<Self::HandleType>;
 }
 
+/// `HandleProvider`s on the heap are `HandleProvider`s too.
+impl<P: HandlesProvider> HandlesProvider for Box<P> {
+    type HandleType = P::HandleType;
+
+    fn get_handles(&mut self, waker: &Arc<Waker>) -> Option<Self::HandleType> {
+        self.as_mut().get_handles(waker)
+    }
+
+    fn get_suitable_buffer_for<'a, Q>(
+        &self,
+        handles: &Self::HandleType,
+        queue: &'a Q,
+    ) -> Result<
+        <Q as CaptureQueueableProvider<'a, Self::HandleType>>::Queueable,
+        GetSuitableBufferError,
+    >
+    where
+        Q: GetCaptureBufferByIndex<'a, Self::HandleType>
+            + GetFreeCaptureBuffer<'a, Self::HandleType>,
+    {
+        self.as_ref().get_suitable_buffer_for(handles, queue)
+    }
+}
+
 pub struct MMAPProvider(Vec<MMAPHandle>);
 
 impl MMAPProvider {
