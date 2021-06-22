f0b7c12bf645b75c99047638f2efd9dd4c3dd069
Alexandre Courbot <gnurou@gmail.com>
2021-06-22 17:05:09 +0900

    fwht_decoder: use MMAP for CAPTURE queue
    Using dmabufs require access to the dma_heap, which may not be available
    on all systems. MMAP is the default way that V4L2 operates, so use this.

--- lib/examples/fwht_decoder/main.rs
+++ lib/examples/fwht_decoder/main.rs
@@ -13,10 +13,7 @@ use anyhow::ensure;
 use nix::sys::time::{TimeVal, TimeValLike};
 use v4l2r::{
     decoder::{format::fwht::FwhtFrameParser, FormatChangedReply},
-    device::queue::{
-        handles_provider::{PooledHandles, PooledHandlesProvider},
-        FormatBuilder,
-    },
+    device::queue::{handles_provider::MmapProvider, FormatBuilder},
     memory::{MemoryType, MmapHandle},
     PlaneLayout,
 };
@@ -33,7 +30,6 @@ use v4l2r::{
         poller::PollError,
         queue::{direction::Capture, dqbuf::DqBuffer},
     },
-    memory::{DmaBufHandle, DmaBufferHandles},
     Format, Rect,
 };
 
@@ -112,67 +108,60 @@ fn main() {
     let poll_count_writer = Arc::clone(&poll_count_reader);
     let start_time = std::time::Instant::now();
     let mut frame_counter = 0usize;
-    let mut output_ready_cb =
-        move |mut cap_dqbuf: DqBuffer<Capture, PooledHandles<DmaBufferHandles<File>>>| {
-            let bytes_used = cap_dqbuf.data.get_first_plane().bytesused() as usize;
-            // Ignore zero-sized buffers.
-            if bytes_used == 0 {
-                return;
-            }
+    let mut output_ready_cb = move |cap_dqbuf: DqBuffer<Capture, Vec<MmapHandle>>| {
+        let bytes_used = cap_dqbuf.data.get_first_plane().bytesused() as usize;
+        // Ignore zero-sized buffers.
+        if bytes_used == 0 {
+            return;
+        }
 
-            let elapsed = start_time.elapsed();
-            frame_counter += 1;
-            let fps = frame_counter as f32 / elapsed.as_millis() as f32 * 1000.0;
-            let ppf = poll_count_reader.load(Ordering::SeqCst) as f32 / frame_counter as f32;
-            print!(
-                "\rDecoded buffer {:#5}, index: {:#2}), bytes used:{:#6} fps: {:#5.2} ppf: {:#4.2}",
-                cap_dqbuf.data.sequence(),
-                cap_dqbuf.data.index(),
-                bytes_used,
-                fps,
-                ppf,
-            );
-            io::stdout().flush().unwrap();
+        let elapsed = start_time.elapsed();
+        frame_counter += 1;
+        let fps = frame_counter as f32 / elapsed.as_millis() as f32 * 1000.0;
+        let ppf = poll_count_reader.load(Ordering::SeqCst) as f32 / frame_counter as f32;
+        print!(
+            "\rDecoded buffer {:#5}, index: {:#2}), bytes used:{:#6} fps: {:#5.2} ppf: {:#4.2}",
+            cap_dqbuf.data.sequence(),
+            cap_dqbuf.data.index(),
+            bytes_used,
+            fps,
+            ppf,
+        );
+        io::stdout().flush().unwrap();
 
-            if let Some(ref mut output) = output_file {
-                let pooled_handles = cap_dqbuf.take_handles().unwrap();
-                let handles = pooled_handles.handles();
-                let mapping = handles[0].map().expect("Failed to map capture buffer");
-                output
-                    .write_all(&mapping)
-                    .expect("Error while writing output data");
-            }
-        };
-    let decoder_event_cb =
-        move |event: DecoderEvent<PooledHandlesProvider<Vec<DmaBufHandle<File>>>>| match event {
-            DecoderEvent::FrameDecoded(dqbuf) => output_ready_cb(dqbuf),
-            DecoderEvent::EndOfStream => (),
-        };
-    type PooledDmaBufHandlesProvider = PooledHandlesProvider<Vec<DmaBufHandle<File>>>;
-    let set_capture_format_cb =
-        move |f: FormatBuilder,
-              visible_rect: Rect,
-              min_num_buffers: usize|
-              -> anyhow::Result<FormatChangedReply<PooledDmaBufHandlesProvider>> {
-            // Let's keep the pixel format that the decoder found convenient.
-            let format = f.format();
+        if let Some(ref mut output) = output_file {
+            let mapping = cap_dqbuf
+                .get_plane_mapping(0)
+                .expect("Failed to map capture buffer");
+            output
+                .write_all(&mapping)
+                .expect("Error while writing output data");
+        }
+    };
+    let decoder_event_cb = move |event: DecoderEvent<MmapProvider>| match event {
+        DecoderEvent::FrameDecoded(dqbuf) => output_ready_cb(dqbuf),
+        DecoderEvent::EndOfStream => (),
+    };
+    let set_capture_format_cb = move |f: FormatBuilder,
+                                      visible_rect: Rect,
+                                      min_num_buffers: usize|
+          -> anyhow::Result<FormatChangedReply<MmapProvider>> {
+        // Let's keep the pixel format that the decoder found convenient.
+        let format = f.format();
 
-            println!(
-                "New CAPTURE format: {:?} (visible rect: {})",
-                format, visible_rect
-            );
+        println!(
+            "New CAPTURE format: {:?} (visible rect: {})",
+            format, visible_rect
+        );
 
-            let dmabuf_fds: Vec<Vec<_>> =
-                utils::dmabuf_exporter::export_dmabufs(format, min_num_buffers).unwrap();
-
-            Ok(FormatChangedReply {
-                provider: PooledHandlesProvider::new(dmabuf_fds),
-                // TODO: can't the provider report the memory type that it is
-                // actually serving itself?
-                mem_type: MemoryType::DmaBuf,
-                num_buffers: min_num_buffers,
-            })
-        };
+        Ok(FormatChangedReply {
+            provider: MmapProvider::new(format),
+            // TODO: can't the provider report the memory type that it is
+            // actually serving itself?
+            mem_type: MemoryType::Mmap,
+            num_buffers: min_num_buffers,
+        })
+    };
 
     let mut decoder = Decoder::open(Path::new(device_path))
         .expect("Failed to open device")
