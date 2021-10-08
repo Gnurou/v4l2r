52e7226ba888b7d93ccdacb472631d52643461c8
Alexandre Courbot <gnurou@gmail.com>
2021-10-09 06:50:53 +0900

    lib/bindings: silence compiler warnings
    Since rustc 1.56 we are getting extra warnings on the bindings unit
    tests. Disable them.

--- lib/src/bindings.rs
+++ lib/src/bindings.rs
@@ -2,6 +2,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(deref_nullptr)]
+#![allow(unaligned_references)]
 #![allow(clippy::all)]
 
 #[cfg(target_pointer_width = "64")]
