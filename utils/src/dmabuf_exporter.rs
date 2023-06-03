cb247e5dc8c54825a655c4e8cdd16550c763a32a
Alexandre Courbot <gnurou@gmail.com>
2023-06-03 16:22:50 +0900

    update dma-heap to 0.2.1

--- utils/src/dmabuf_exporter.rs
+++ utils/src/dmabuf_exporter.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
 
-use dma_heap::{DmaBufHeap, DmaBufHeapType};
+use dma_heap::{Heap, HeapKind};
 use v4l2r::{memory::DmaBufHandle, Format};
 
 use anyhow::Result;
 
 pub fn export_dmabufs(format: &Format, nb_buffers: usize) -> Result<Vec<Vec<DmaBufHandle<File>>>> {
-    let heap = DmaBufHeap::new(DmaBufHeapType::System).unwrap();
+    let heap = Heap::new(HeapKind::System)?;
 
     let fds: Vec<Vec<DmaBufHandle<File>>> = (0..nb_buffers)
         .map(|_| {
@@ -14,7 +14,8 @@ pub fn export_dmabufs(format: &Format, nb_buffers: usize) -> Result<Vec<Vec<DmaB
                 .plane_fmt
                 .iter()
                 .map(|plane| {
-                    DmaBufHandle::from(heap.allocate::<File>(plane.sizeimage as usize).unwrap())
+                    let fd = File::from(heap.allocate(plane.sizeimage as usize).unwrap());
+                    DmaBufHandle::from(fd)
                 })
                 .collect()
         })
