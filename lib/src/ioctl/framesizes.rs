4378858a9edc3be3d9b82939565a8f5681a3cf8b
Albert Esteve <aesteve@redhat.com>
2023-09-25 15:11:27 +0200

    ioctl/framesizes: add v4l2_frmsizeenum accessor
    Add an accessor for v4l2_frmsizeenum anonymous
    union members, avoiding having to write the union
    name and safely returning the appropiate member of
    the union that is used.
    
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- lib/src/ioctl/framesizes.rs
+++ lib/src/ioctl/framesizes.rs
@@ -13,6 +13,40 @@ impl FrameSize for bindings::v4l2_frmsizeenum {
     }
 }
 
+/// A wrapper for the 'v4l2_frmsizeenum' union member types
+#[derive(Debug)]
+pub enum FrmSizeTypes<'a> {
+    Discrete(&'a bindings::v4l2_frmsize_discrete),
+    StepWise(&'a bindings::v4l2_frmsize_stepwise),
+}
+
+impl bindings::v4l2_frmsizeenum {
+    /// Safely access the size member of the struct based on the
+    /// returned index.
+    pub fn size(&self) -> Option<FrmSizeTypes> {
+        match self.index {
+            // SAFETY: the member of the union that gets used by the driver
+            // is determined by the index
+            bindings::v4l2_frmsizetypes_V4L2_FRMSIZE_TYPE_DISCRETE => {
+                Some(FrmSizeTypes::Discrete(unsafe {
+                    &self.__bindgen_anon_1.discrete
+                }))
+            }
+
+            // SAFETY: the member of the union that gets used by the driver
+            // is determined by the index
+            bindings::v4l2_frmsizetypes_V4L2_FRMSIZE_TYPE_CONTINUOUS
+            | bindings::v4l2_frmsizetypes_V4L2_FRMSIZE_TYPE_STEPWISE => {
+                Some(FrmSizeTypes::StepWise(unsafe {
+                    &self.__bindgen_anon_1.stepwise
+                }))
+            }
+
+            _ => None,
+        }
+    }
+}
+
 #[doc(hidden)]
 mod ioctl {
     use crate::bindings::v4l2_frmsizeenum;
