b10b780f1f795b24fa59bf9558aff4207b4e2fc7
Alexandre Courbot <gnurou@gmail.com>
2024-07-04 23:51:26 +0900

    utils: replace DMABUF heap with memfd
    Memfd does not require access to a device and is simpler to use overall.
    Also allows us to remove a dependency.

--- utils/src/dmabuf_exporter.rs
+++ utils/src/dmabuf_exporter.rs
@@ -1,21 +1,24 @@
 use std::fs::File;
 
-use dma_heap::{Heap, HeapKind};
+use nix::{
+    sys::memfd::{memfd_create, MemFdCreateFlag},
+    unistd::ftruncate,
+};
 use v4l2r::{memory::DmaBufHandle, Format};
 
 use anyhow::Result;
 
 pub fn export_dmabufs(format: &Format, nb_buffers: usize) -> Result<Vec<Vec<DmaBufHandle<File>>>> {
-    let heap = Heap::new(HeapKind::System)?;
-
     let fds: Vec<Vec<DmaBufHandle<File>>> = (0..nb_buffers)
         .map(|_| {
             format
                 .plane_fmt
                 .iter()
                 .map(|plane| {
-                    let fd = File::from(heap.allocate(plane.sizeimage as usize).unwrap());
-                    DmaBufHandle::from(fd)
+                    memfd_create(c"memfd buffer", MemFdCreateFlag::MFD_ALLOW_SEALING)
+                        .and_then(|fd| ftruncate(&fd, i64::from(plane.sizeimage)).map(|_| fd))
+                        .map(|fd| DmaBufHandle::from(File::from(fd)))
+                        .unwrap()
                 })
                 .collect()
         })
